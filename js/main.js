7d17903396217b5c12140c157c09d9c20821ad22
Audrey Strelzoff <astrelzoff@ufl.edu>
2017-11-04 21:42:43 -0500

    Add event listener to return h1 to default color on mouse leave

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@
 // Create global variable hoverBanner and set to the header area
 var hoverBanner = document.getElementsByTagName('header')[0];
 
-// Add even lister to hoverBanner that listens for a mouse enter
+// Add event listener to hoverBanner that listens for a mouse enter
 hoverBanner.addEventListener('mouseenter', function changeBanner() {
     // On mouseenter, add the class jazzedColor to the header text
     var color = document.getElementById('headerP');
     color.classList.add('jazzedColor');
+});
+
+// EVENT 2: chnage header title color back on mouse leave
+// Add event listener to hoverBanner that listens for a mouse leave
+hoverBanner.addEventListener('mouseleave', function () {
+    // On mouseleave, remove the class jazzedColor to the header text
+    var rmcolor = document.getElementById('headerP');
+    rmcolor.classList.remove('jazzedColor');
 });
